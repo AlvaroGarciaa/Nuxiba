7831eb62328ac0e78d1f329af76b3390e3e0c968
Alvaro <ag587635@gmail.com>
2024-09-19 11:41:34 -0600

    Mejoras al diseño con Material - UI

--- nuxiba-test/src/components/UserDetail.js
+++ nuxiba-test/src/components/UserDetail.js
@@ -1,17 +1,54 @@
 import React from 'react';
+import { Checkbox, FormControlLabel, Button, TextField } from '@mui/material';
+import { List, ListItem, ListItemText } from '@mui/material';
+
 
 const UserDetail = ({ user, onShowPosts, onShowTodos, showPosts, showTodos, posts, todos, onAddTodo }) => (
-  <div>
+  <div style={{color: 'white'}}>
+    <div style={{textAlign: 'center', marginBottom: '20px' ,marginTop:'20px'}} >
     <h2>{user.name}</h2>
     <p>Username: {user.username}</p>
     <p>Email: {user.email}</p>
-    <button onClick={onShowPosts}>Posts</button>
-    <button onClick={onShowTodos}>Todos</button>
+    <div style={{marginTop: '20px'}}> 
+      <Button variant="contained" color="primary" onClick={onShowPosts} style={{marginRight: '20px'}}>
+        Posts
+      </Button>
+      <Button variant="contained" color="secondary" onClick={onShowTodos}>
+        Todos
+      </Button>
+    </div>
+    </div>
+   
 
-    {showPosts && (
+    <div style={{margin: '20px'}}> 
+
+    {/* <button onClick={onShowPosts} style={{marginRight: '20px'}}>Posts</button>
+    <button onClick={onShowTodos}>Todos</button> */}
+
+    
+    </div>
+    
+      <div textAlign = 'center'>
+
+      {showPosts && (
+  <div>
+    <h3 style={{paddingLeft: '15px'}}>Posts</h3>
+    <List sx={{ paddingTop: '15px' }}>
+      {posts.map((post) => (
+        <ListItem key={post.id}>
+          <ListItemText
+            primary={post.title}
+            secondary={post.body}
+          />
+        </ListItem>
+      ))}
+    </List>
+  </div>
+)}
+    {/* {showPosts && (
       <div>
         <h3>Posts</h3>
-        <ul>
+        <ul style= {{paddingTop: '15px'}}>
           {posts.map((post) => (
             <li key={post.id}>
               <h4>{post.title}</h4>
@@ -20,34 +57,46 @@ const UserDetail = ({ user, onShowPosts, onShowTodos, showPosts, showTodos, post
           ))}
         </ul>
       </div>
-    )}
+    )} */}
 
-    {showTodos && (
-      <div>
-        <h3>Todos</h3>
-        <ul>
-          {todos.sort((a, b) => b.id - a.id).map((todo) => (
-            <li key={todo.id}>
-              <input type="checkbox" checked={todo.completed} readOnly />
-              {todo.title}
-            </li>
-          ))}
-        </ul>
 
-        <h3>Add New Todo</h3>
-        <form onSubmit={onAddTodo}>
-          <label>
-            Title:
-            <input type="text" name="title" required />
-          </label>
-          <label>
-            Completed:
-            <input type="checkbox" name="completed" />
-          </label>
-          <button type="submit">Save</button>
-        </form>
-      </div>
-    )}
+{showTodos && (
+  <div style={{}}>
+    <h3>Todos</h3>
+    <ul style={{ paddingTop: '15px',  listStyleType: 'none'}}>
+      {todos.sort((a, b) => b.id - a.id).map((todo) => (
+        <li key={todo.id}>
+          <FormControlLabel
+            control={<Checkbox checked={todo.completed} readOnly />}
+            label={todo.title}
+          />
+        </li>
+      ))}
+    </ul>
+    <div style={{paddingTop: '25px'}}>
+    <h3>Add New Todo</h3>
+    <form onSubmit={onAddTodo}>
+      <TextField
+        label="Title"
+        name="title"
+        variant="outlined"
+        required
+        fullWidth
+        margin="normal"
+      />
+      <FormControlLabel
+        control={<Checkbox name="completed" />}
+        label="Completed"
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Save
+      </Button>
+    </form>
+    </div>
+  </div>
+ 
+)}
+    </div>
   </div>
 );
 
